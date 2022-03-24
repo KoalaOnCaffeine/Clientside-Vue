2a7d289c19b4c399565ef0f9e09e0f71a2df9908
Tom <tomtomnewton2005@gmail.com>
2022-03-24 16:15:24 +0000

    Change titles to Sign up and Log in, without 'page'

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
         'vuetify'
     ],
     pages: {
-        'home': {
+        home: {
             entry: 'src/pages/home/main.js',
             template: 'public/home.html',
             filename: 'home.html',
@@ -14,13 +14,13 @@ module.exports = defineConfig({
             entry: 'src/pages/login/main.js',
             template: 'public/home.html',
             filename: 'login.html',
-            title: 'Login Page'
+            title: 'Login'
         },
         signup: {
             entry: 'src/pages/signup/main.js',
             template: 'public/home.html',
             filename: 'signup.html',
-            title: 'Sign up Page'
+            title: 'Sign up'
         }
     }
 })
