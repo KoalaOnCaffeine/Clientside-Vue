969e7717d900fe87d6f896a134e288391505a825
tomto <tomtomnewton2005@gmail.com>
2022-04-18 14:16:00 +0100

    Add the visuals to the login page

--- src/pages/login/LogInPage.vue
+++ src/pages/login/LogInPage.vue
@@ -1,18 +1,79 @@
 <template>
-  <V-app>
+  <v-app>
     <TopBar/>
-  </V-app>
+
+    <v-app>
+      <TopBar/>
+      <v-spacer/>
+      <h1 class="align-self-center">Log in</h1>
+      <v-spacer/>
+      <v-container>
+        <v-col>
+          <v-row>
+            <v-spacer/>
+            <v-text-field
+                label="Username"
+                v-model="username"
+                outlined
+                prepend-icon="mdi-account"
+                autofocus>
+            </v-text-field>
+            <v-spacer/>
+          </v-row>
+
+          <v-row>
+            <v-spacer/>
+            <v-text-field
+                :type="showPassword ? 'text' : 'password'"
+                v-model="password"
+                label="Password"
+                outlined
+                prepend-icon="mdi-key"
+                :append-icon="showPassword ? 'mdi-eye':'mdi-eye-off'"
+                @click:append="showPassword ^= true">
+            </v-text-field>
+            <v-spacer/>
+          </v-row>
+
+          <v-row>
+            <v-spacer/>
+            <v-btn @click="submitLogin(username, password)">Log in</v-btn>
+            <v-spacer/>
+          </v-row>
+        </v-col>
+      </v-container>
+      <v-spacer/>
+      <v-spacer/>
+
+    </v-app>
+  </v-app>
 </template>
 
 <script>
 import TopBar from '../../common-components/TopBar.vue'
 
+function submitLogin(username, password) {
+  alert(username, password) // TODO
+}
+
 export default {
   name: "LogInPage",
-  components: {TopBar}
+  components: {TopBar},
+  data: () => ({
+    showPassword: false,
+    username: '',
+    password: ''
+  }),
+  methods: {
+    submitLogin: submitLogin
+  }
 }
 </script>
 
 <style scoped>
+
+.v-text-field {
+  width: 15%;
+}
 
 </style>
