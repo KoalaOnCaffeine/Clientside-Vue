5cd5bfcbfc21b4a7908556c67f5702b6db3a40b5
tomto <tomtomnewton2005@gmail.com>
2022-04-30 23:07:51 +0100

    Create a test in the dashboard that will fail because you can't find your username without decoding the whole thing

--- src/pages/dashboard/DashboardPage.vue
+++ src/pages/dashboard/DashboardPage.vue
@@ -1,10 +1,34 @@
 <template>
-  <div>Placeholder</div>
+  <div>
+    <template v-if="loadingUsername">
+      <p>Loading...</p>
+    </template>
+    <template v-else>
+      Hello, {{ username }} - sent the auth token which is wrong
+    </template>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "DashboardPage"
+  name: 'DashboardPage',
+  data: async () => ({
+    username: '',
+    loadingUsername: true
+  }),
+  methods: {
+    getUsername: () => {
+      fetch(`/api/accounts/${localStorage.getItem('AuthToken')}`).then(res => {
+        res.json()
+      }).then(json => {
+        this.username = json.username
+        this.loadingUsername = false
+      })
+    }
+  },
+  created() {
+    this.getUsername()
+  }
 }
 </script>
 
