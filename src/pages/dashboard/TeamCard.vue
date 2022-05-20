db169539720e2f1dd9c8c5e674a2adc1898d47f6
tomto <tomtomnewton2005@gmail.com>
2022-05-20 17:16:31 +0100

    Work towards a nice looking team card

---
+++ src/pages/dashboard/TeamCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card height="500" width="500">
+    <v-card-title style="position:absolute;">{{ name }}</v-card-title>
+    <v-img :src="image" height="100" style="opacity: 35%"></v-img>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "TeamCard",
+  props: ['name', 'image', 'description', 'members', 'projects', 'id'],
+}
+</script>
+
+<style scoped>
+
+</style>
