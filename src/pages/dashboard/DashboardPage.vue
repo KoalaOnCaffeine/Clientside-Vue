b624c5df6829088ade308ce531cf3e582089f0a9
tomto <tomtomnewton2005@gmail.com>
2022-05-20 17:17:04 +0100

    Add test team cards

--- src/pages/dashboard/DashboardPage.vue
+++ src/pages/dashboard/DashboardPage.vue
@@ -6,15 +6,46 @@
     <template v-else>
       {{ teamResponse }}
     </template>
+
+    <template v-for="team in testTeams">
+      <TeamCard :id='team.id'
+                :key='team.id'
+                :description='team.description'
+                :image='team.image'
+                :members='team.members'
+                :name='team.name '
+                :projects='team.projects'/>
+      <v-spacer :key="team.id"/>
+    </template>
   </div>
 </template>
 
 <script>
+import TeamCard from "@/pages/dashboard/TeamCard";
+
 export default {
   name: 'DashboardPage',
+  components: {TeamCard},
   data: () => ({
     teamResponse: '',
-    loadingTeams: true
+    loadingTeams: true,
+    testTeams: [
+      {
+        name: 'Name',
+        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        description: 'Short description',
+        members: ['0123456789', '0123456789'],
+        projects: ['0123456789', '0123456789'],
+        id: '0123456789'
+      }, {
+        name: 'Name2',
+        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        description: '2 Short description',
+        members: ['20123456789', '20123456789'],
+        projects: ['20123456789', '20123456789'],
+        id: '20123456789'
+      }
+    ]
   }),
   methods: {
     getTeams: function () {
