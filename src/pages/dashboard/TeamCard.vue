3d5114a0cc32427e4948d9b0472aa6d33814e868
tomto <tomtomnewton2005@gmail.com>
2022-05-25 22:39:17 +0100

    Create the team card, using the card header. Projects will be arranged into upto 3 rows, each with 2 columns, prioritising filling rows

---
+++ src/pages/dashboard/TeamCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="ma-4" height="400" max-height="75%" max-width="75%" outlined width="400">
+    <CardHeader :description="description" :image="image" :loaded-members="loadedMembers" :name="name"/>
+    <!-- Project list -->
+    <v-container style="text-align: center; height: 65%">
+      <!-- 3 rows -->
+      <v-row v-for="i in 3" :key="i" class="text-left">
+        <!-- Each with 2 columns -->
+        <v-col v-for="j in 2" :key=j cols="6">
+          {{ 2 * (i - 1) + (j - 1) }}
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+import CardHeader from "@/pages/dashboard/CardHeader";
+
+export default {
+  name: "TeamCard",
+  components: {CardHeader},
+  props: ['id', 'name', 'description', 'image', 'members', 'projects'],
+  data: () => ({
+    loadedMembers: [
+      {'id': '0123456789', 'name': 'elizabeth olsen'},
+      {'id': '0123456777', 'name': 'chris hemsworth'},
+      {'id': '0123456777', 'name': 'rafe cameron'}],
+    loadedProjects: [
+      {name: 'asd', description: 'dkjlljsjkl'}
+    ]
+  }),
+  mounted() {
+    this.getTeam()
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
