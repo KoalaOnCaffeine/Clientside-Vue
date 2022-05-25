da7731173c6710f59b3428ab351f0002c24cdb0f
tomto <tomtomnewton2005@gmail.com>
2022-05-25 22:57:32 +0100

    Updated commenting

--- src/pages/dashboard/CardHeader.vue
+++ src/pages/dashboard/CardHeader.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- The title should float on top of the image -->
     <v-card-title style="position:absolute;">{{ name }}</v-card-title>
     <v-img :src="image" class="rounded-image" height="100" style="opacity: 45%"></v-img>
     <!-- Description and user icons -->
@@ -15,13 +16,15 @@
         <v-col cols="4">
           <v-container>
             <v-row>
+              <!-- Load the lower value between the number of members and 3, which also prevents trying to load when there are no members -->
               <v-col v-for="i in Math.min(loadedMembers.length, 3)" :key="i" cols="3">
                 <v-tooltip bottom>
                   <template v-slot:activator="{ on, attrs }">
+                    <!-- v-for i in 3 uses values 1, 2 and 3, so subtract one to get the right value -->
                     <v-icon v-bind="attrs" @click="redirectToProfile(loadedMembers[i-1].id)" v-on="on">mdi-account
                     </v-icon>
                   </template>
-                  <!-- v-for i in 3 uses values 1, 2 and 3, so subtract one to get the right value -->
+                  <!-- Show the member's name when you hover -->
                   <span>{{ loadedMembers[i - 1].name }}</span>
                 </v-tooltip>
               </v-col>
@@ -46,6 +49,7 @@ export default {
 </script>
 
 <style scoped>
+/* The image's top half should be rounded to nicely fit within the card */
 .rounded-image {
   border-top-left-radius: 40px;
   border-top-right-radius: 40px;
