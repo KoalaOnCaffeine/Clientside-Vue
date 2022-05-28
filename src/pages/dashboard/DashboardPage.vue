63b64b3933b7ce001c7a63f7d8e99b9ebd6721d2
tomto <tomtomnewton2005@gmail.com>
2022-05-28 23:28:04 +0100

    Include test data and cards for checking the cards

--- src/pages/dashboard/DashboardPage.vue
+++ src/pages/dashboard/DashboardPage.vue
@@ -7,7 +7,7 @@
       {{ teamResponse }}
     </template>
 
-    <ProjectCard v-for="team in teamResponse" :key="team.id"
+    <ProjectCard v-for="team in testProjects" :key="team.id"
                  :description='team.description'
                  :image='team.image'
                  :members='team.members'
@@ -15,41 +15,62 @@
                  :projects='team.projects'
                  class="rounded-card"/>
 
-    <ProjectCard v-for="team in testTeams" :id='team.id'
-                 :key='team.id'
-                 :description='team.description'
-                 :image='team.image'
-                 :members='team.members'
-                 :name='team.name '
-                 :projects='team.projects'
-                 class="rounded-card"/>
+    <TeamCard v-for="team in testTeams"
+              :id='team.id'
+              :key='team.id'
+              :description='team.description'
+              :image='team.image'
+              :members='team.members'
+              :name='team.name '
+              :projects='team.projects'
+              class="rounded-card"/>
   </v-app>
 </template>
 
 <script>
+import TeamCard from "@/pages/dashboard/TeamCard";
 import ProjectCard from "@/pages/dashboard/ProjectCard";
 
 export default {
   name: 'DashboardPage',
-  components: {ProjectCard},
+  components: {ProjectCard, TeamCard},
   data: () => ({
     teamResponse: '',
     loadingTeams: true,
+    testProjects: [
+      {
+        id: '0123456789',
+        name: 'Project 1',
+        description: 'Project 1 description',
+        members: ['20123456789', '20123456789'],
+        cards: ['20123456789', '20123456789'],
+        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+      },
+      {
+        id: '20123456789',
+        name: 'Project 1',
+        description: 'Project 1 description',
+        members: ['20123456789', '20123456789'],
+        projects: ['20123456789', '20123456789'],
+        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+      }
+    ],
     testTeams: [
       {
-        name: 'Name',
-        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
-        description: 'Short description',
+        id: '0123456789',
+        name: 'Team 1',
+        description: 'Team 1 description',
         members: ['0123456789', '0123456789'],
         projects: ['0123456789', '0123456789'],
-        id: '0123456789'
-      }, {
-        name: 'Name2',
-        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U'
+      },
+      {
+        id: '20123456789',
+        name: 'Team 2',
         description: '2 Short description',
         members: ['20123456789', '20123456789'],
         projects: ['20123456789', '20123456789'],
-        id: '20123456789'
+        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U'
       }
     ]
   }),
