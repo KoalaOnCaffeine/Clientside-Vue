fe4b13bc877e2f2bd5bac7a0f7d588b3a90013c3
Tom <tomtomnewton2005@gmail.com>
2022-05-30 23:34:57 +0100

    Remove the test parts for the team and project card, and replace it with a team card that loads from the API using loadTeams

--- src/pages/dashboard/DashboardPage.vue
+++ src/pages/dashboard/DashboardPage.vue
@@ -4,91 +4,43 @@
       <p>Loading...</p>
     </template>
     <template v-else>
-      {{ teamResponse }}
+      <TeamCard v-for="team in teams" :id="team.id" :key="team.id"
+                :description="team.description"
+                :image="team.image"
+                :members="team.memberIDs"
+                :name="team.name"
+                :projects="team.projectIDs"
+                class="rounded-card"
+      />
     </template>
 
-    <ProjectCard v-for="team in testProjects" :key="team.id"
-                 :description='team.description'
-                 :image='team.image'
-                 :members='team.members'
-                 :name='team.name '
-                 :projects='team.projects'
-                 class="rounded-card"/>
-
-    <TeamCard v-for="team in testTeams"
-              :id='team.id'
-              :key='team.id'
-              :description='team.description'
-              :image='team.image'
-              :members='team.members'
-              :name='team.name '
-              :projects='team.projects'
-              class="rounded-card"/>
   </v-app>
 </template>
 
 <script>
 import TeamCard from "@/pages/dashboard/TeamCard";
-import ProjectCard from "@/pages/dashboard/ProjectCard";
 
 export default {
   name: 'DashboardPage',
-  components: {ProjectCard, TeamCard},
+  components: {TeamCard},
   data: () => ({
-    teamResponse: '',
+    teams: [],
     loadingTeams: true,
-    testProjects: [
-      {
-        id: '0123456789',
-        name: 'Project 1',
-        description: 'Project 1 description',
-        members: ['20123456789', '20123456789'],
-        cards: ['20123456789', '20123456789'],
-        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
-      },
-      {
-        id: '20123456789',
-        name: 'Project 1',
-        description: 'Project 1 description',
-        members: ['20123456789', '20123456789'],
-        projects: ['20123456789', '20123456789'],
-        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
-      }
-    ],
-    testTeams: [
-      {
-        id: '0123456789',
-        name: 'Team 1',
-        description: 'Team 1 description',
-        members: ['0123456789', '0123456789'],
-        projects: ['0123456789', '0123456789'],
-        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U'
-      },
-      {
-        id: '20123456789',
-        name: 'Team 2',
-        description: '2 Short description',
-        members: ['20123456789', '20123456789'],
-        projects: ['20123456789', '20123456789'],
-        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U'
-      }
-    ]
   }),
   methods: {
-    getTeams: function () {
-
+    loadTeams: function () {
       fetch('/api/teams/', {
         headers: {
           'Authorization': `Bearer ${localStorage.getItem("AuthToken")}`
         }
       }).then(res => res.json()).then(json => {
+        this.teams = json
         this.loadingTeams = false
-        this.teamResponse = json
       })
     }
   },
   mounted() {
-    this.getTeams()
+    this.loadTeams()
   }
 }
 </script>
